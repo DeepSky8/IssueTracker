aefedcd2d46ba18956cea753d6b490e4683a014f
Benjamin Kyrlach <bkyrlach@gmail.com>
2015-03-04 19:05:24 -0500

    Added basic support for creating issues. Many improvements needed here.

---
+++ src/main/java/com/kyrlach/issuetracker/issue/IssueController.java
@@ -0,0 +1,35 @@
+package com.kyrlach.issuetracker.issue;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/issues")
+public class IssueController {
+	
+	@Autowired
+	private IssueRepository issueRepository;
+	
+	@RequestMapping(method = RequestMethod.GET)
+    public String issues(Model model) {
+    	model.addAttribute("issues", issueRepository.findAll());
+    	return "issueList";
+    }
+	
+	@RequestMapping(value = "/new", method = RequestMethod.GET) 
+	public String newIssue(Model model) {
+		model.addAttribute("issueForm", new IssueForm());
+		return "newIssue";
+	}
+	
+	@RequestMapping(value = "/new", method = RequestMethod.POST)
+	public String saveIssue(@ModelAttribute IssueForm issueForm, Model model) {
+		Issue newIssue = new Issue(issueForm.getTitle(), issueForm.getDescription());
+		issueRepository.save(newIssue);
+		return "redirect:/issues";
+	}
+}
