3df77f2a884c33d2a38900c92698ab71694b0b53
TNT0677 <thomas.truesdell@ccuniversity.edu>
2015-03-12 19:20:54 -0400

    Added code to check user inputs of username and password against the database to allow users to log into the site.

--- src/main/java/com/kyrlach/issuetracker/Application.java
+++ src/main/java/com/kyrlach/issuetracker/Application.java
@@ -1,13 +1,28 @@
 package com.kyrlach.issuetracker;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class Application {
+import com.kyrlach.issuetracker.login.User;
+import com.kyrlach.issuetracker.login.UserRepository;
 
+@SpringBootApplication
+public class Application implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository userRepository;
+	
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
+	@Override
+	public void run(String... arg0) throws Exception {
+
+		userRepository.save(new User("Tom", "Test"));
+		userRepository.save(new User("Ben", "Secr3t"));
+		userRepository.save(new User("Joel", "1234"));
+	}
 }
--- src/main/java/com/kyrlach/issuetracker/login/LoginController.java
+++ src/main/java/com/kyrlach/issuetracker/login/LoginController.java
@@ -1,19 +1,27 @@
 package com.kyrlach.issuetracker.login;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.thymeleaf.expression.Strings;
 
 @Controller
 @RequestMapping("/")
 public class LoginController {
 	
 	private static Logger logger = LogManager.getLogger(LoginController.class);
-			
+	
+	@Autowired
+	private UserRepository userRepository;
+	
 	@RequestMapping(method = RequestMethod.GET)
 	public String welcome(Model model) {
 		model.addAttribute("loginForm", new LoginForm());
@@ -22,7 +30,23 @@ public class LoginController {
 	
 	@RequestMapping(method = RequestMethod.POST)
 	public String login(@ModelAttribute LoginForm loginForm, Model model) {
-		logger.info(loginForm);
-		return "login";
-	}
-}
+			User credentailCheck = new User(loginForm.getUsername(), loginForm.getPassword());
+			logger.info(loginForm);
+			User user = userRepository.findByUsername(credentailCheck.getUsername());
+			logger.info(user);
+			if(user == null) {
+				List<String> errors = new ArrayList<String>();
+				errors.add("Wrong username/password");
+				model.addAttribute("errors", errors);
+				return "login";
+			} else {
+				if(user.getPassword().equals(credentailCheck.getPassword())) {
+					return "redirect:/issues";
+				} else {
+					List<String> errors = new ArrayList<String>();
+					errors.add("Wrong username/password");
+					model.addAttribute("errors", errors);
+					return "login";
+				}
+			}
+	}}
