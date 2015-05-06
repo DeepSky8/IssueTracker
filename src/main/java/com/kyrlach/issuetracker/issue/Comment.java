8f4dbda5bd9152514854fd19de4a8c2a7059cab2
Benjamin Kyrlach <bkyrlach@gmail.com>
2015-05-05 20:23:17 -0400

    More WIP for adding comments to issues.

--- src/main/java/com/kyrlach/issuetracker/issue/Comment.java
+++ src/main/java/com/kyrlach/issuetracker/issue/Comment.java
@@ -1,5 +1,50 @@
 package com.kyrlach.issuetracker.issue;
 
-public class Comment {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
+import com.kyrlach.issuetracker.login.User;
+
+@Entity
+public class Comment {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private long id;
+	
+	@ManyToOne 
+	private User author;
+	
+	private String comment;
+	
+	public Comment() {}
+	
+	public Comment(User author, String comment) {
+		this.author = author;
+		this.comment = comment;
+	}
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	public User getAuthor() {
+		return author;
+	}
+	public void setAuthor(User author) {
+		this.author = author;
+	}
+	
+	public String getComment() {
+		return comment;
+	}
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+	
 }
--- src/main/java/com/kyrlach/issuetracker/issue/Issue.java
+++ src/main/java/com/kyrlach/issuetracker/issue/Issue.java
@@ -1,9 +1,16 @@
 package com.kyrlach.issuetracker.issue;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import org.hibernate.annotations.Cascade;
+import org.hibernate.annotations.CascadeType;
 
 @Entity
 public class Issue {
@@ -14,11 +21,16 @@ public class Issue {
     private String title;
     private String description;
     
+    @OneToMany
+    @Cascade({CascadeType.ALL})
+    private List<Comment> comments;
+    
     public Issue() {}
     
     public Issue(String title, String description) {
     	this.title = title;
     	this.description = description;
+    	this.comments = new ArrayList<Comment>();
     }
 
 	public long getId() {
@@ -41,7 +53,14 @@ public class Issue {
 	public void setDescription(String description) {
 		this.description = description;
 	}
-    
+	
+	public List<Comment> getComments() {
+		return comments;
+	}
+	public void setComments(List<Comment> comments) {
+		this.comments = comments;
+	}
+
 	@Override
     public String toString() {
     	return "Issue("+id+","+title+","+description+")";
